e0b994067b89768d9c9a0d1a3b2d3bbf1bf8ff6b
Gorka Alvarez <galvarez024@ikasle.ehu.eus>
2019-10-06 16:38:05 +0200

    Footer+Credits

--- php/Credits.php
+++ php/Credits.php
@@ -8,7 +8,12 @@
   <section class="main" id="s1">
     <div>
 
-      <h2>DATOS DEL AUTOR/AUTORES</h2>
+      <h2>DATOS DEL AUTOR/AUTORES</h2><br>
+        <p>Gorka Alvarez Marlasca</p>
+        <p>Correo contacto: <a href="mailto:galvarez024@ikasle.ehu.eus" >galvarez024@ikasle.ehu.eus</p>
+        <br>
+        <img src="../images/myAvatar.png" width="125" height="125">
+        <p>Estudiante de Ingenieria Informatica en la especialización <br> de Ingenieria del Software.</p>
 
     </div>
   </section>
