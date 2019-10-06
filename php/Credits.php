0f481492c3a71fc6a1d6c72ac12f045917877e72
iakigarci <garcianoya@hotmail.es>
2019-10-06 16:19:10 +0200

    Casi finalizado parte del form de Question

--- php/Credits.php
+++ php/Credits.php
@@ -10,6 +10,13 @@
 
       <h2>DATOS DEL AUTOR/AUTORES</h2>
 
+      <div>
+      <p><strong>Nombre: </strong> Iñaki Garcia</p>
+      <p><strong>Especialidad: </strong>Ingeniería del Software</p>
+      <p><strong>Email: </strong> <a href="mailto:igarcia361@ikasle.ehu.eus" >igarcia361@ikasle.ehu.eus</a></p>
+      <p><img src="../images/avatarIaki.png" alt=""></p>
+      </div>
+      
     </div>
   </section>
   <?php include '../html/Footer.html' ?>
--- php/QuestionFormWithImage.php
+++ php/QuestionFormWithImage.php
@@ -2,14 +2,58 @@
 <html>
 <head>
   <?php include '../html/Head.html'?>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
 
-      Añadir el formulario y los scripts necesarios para que el usuario <br>
-      pueda introducir los datos de una pregunta con una imagen relacionada con la misma.
+      <form action="" method="get">
+        <div class="form-group">
+          <label for="dirCorreo">Correo electrónico</label>
+            <input type="text" class="form-control" id="dirCorreo" >
+        </div>
+        <div class="form-group">
+          <label for="enunciadoPre">Enunciado de la pregunta</label>
+            <input type="text" class="form-control" id="enunciadoPre" >
+        </div>
+        <div class="form-group">
+          <label for="resCorrecta">Respuesta correcta</label>
+            <input type="text" class="form-control" id="resCorrecta" >
+        </div>
+        <div class="form-group">
+          <label for="resInc1">Respuesta incorrecta 1</label>
+            <input type="text" class="form-control" id="resInc1" >
+        </div>
+        <div class="form-group">
+          <label for="resInc2">Respuesta incorrecta 2</label>
+            <input type="text" class="form-control" id="resInc2" >
+        </div>
+        <div class="form-group">
+          <label for="resInc3">Respuesta incorrecta 3</label>
+            <input type="text" class="form-control" id="resInc3" >
+        </div>
+        <div>
+          <label for="dificultad">Selecciona dificultad</label>
+          <select name="" id="dificultad"> 
+            <option value="baja">Baja</option>
+            <option value="media">Media</option>
+            <option value="dificl">Dificil</option>
+          </select>
+        </div>
+        <div>
+        <label for="tema">Tema</label>
+            <input type="text" class="form-control" id="tema" >
+        </div>
+        <div>
+          <label for="imagen">Selecciona una imagen</label>
+          <input type="file" name="imagen" id="imagen">
+        </div>
+
+        <!-- FALTA QUE APAREZCA LA IMAGEN -->
+ 
+      </form>
 
     </div>
   </section>
