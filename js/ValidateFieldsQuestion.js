c7ac73e1e1ab5bd427cdd2474c309a13fedfb986
iakigarci <garcianoya@hotmail.es>
2019-10-07 13:03:19 +0200

    Regex alumno funciona

--- js/ValidateFieldsQuestion.js
+++ js/ValidateFieldsQuestion.js
@@ -1,32 +1,35 @@
 $(document).ready(function() {
     $('#submit_b').click(function() {
+        var bol = true;
         $("form :input").each(function(){
             var input = $(this);
             if (input.val() == "") {
                 alert("No puede quedar vacio el campo con id: " + input.attr("id"));
             }else if (input.attr("id") == "dirCorreo") {
                 if (comprobarMail($(this).val())) {
-                    alert("TRUE")
+                    alert("Correo correcto")
                 }else{
-                    alert("FALSE");
+                    alert("Correco incorrecto");
+                    bol = false;
                 }
             }else if (input.attr("id") == "enunciadoPre") {
                 var txt = input.val();
-                alert(txt);
                 if (txt.length<10) {
                     alert("Enunciado de pregunta muy corto");
+                    bol = false;
                 }
             } 
         });
+        return bol;
     })
 })
 
 function comprobarMail(mail) {
-    // if (/^[a-zA-Z.!#$%&'*+/=?^_`{|}~-]+(?:[0-9]{3})+@ikasle.ehu.+(eus|es)\W*/.test(mail)){//Alumnos
-    //     return true;
-    // } else {
-    //     return false;
-    // }
-    return true;
+    var regex = /^[a-zA-Z.!#$%&'*+/=?^_`{|}~-]+(?:[0-9]{3})+@ikasle.ehu.+(eus|es)\W*/;
+    if (regex.test(mail)){//Alumnos
+        return true;
+    } else {
+        return false;
+    }
 }
 
--- php/QuestionFormWithImage.php
+++ php/QuestionFormWithImage.php
@@ -12,31 +12,30 @@
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
-
       <form action="" method="get">
         <div >
           <label for="dirCorreo">Correo electrónico</label>
-            <input type="text" id="dirCorreo" >
+            <input type="text" id="dirCorreo" ><br><br>
         </div>
         <div >
           <label for="enunciadoPre">Enunciado de la pregunta</label>
-            <input type="text" id="enunciadoPre" >
+            <input type="text" id="enunciadoPre" ><br><br>
         </div>
         <div >
           <label for="resCorrecta">Respuesta correcta</label>
-            <input type="text" id="resCorrecta" >
+            <input type="text" id="resCorrecta" ><br><br>
         </div>
         <div >
           <label for="resInc1">Respuesta incorrecta 1</label>
-            <input type="text" id="resInc1" >
+            <input type="text" id="resInc1" ><br><br>
         </div>
         <div >
           <label for="resInc2">Respuesta incorrecta 2</label>
-            <input type="text" id="resInc2" >
+            <input type="text" id="resInc2" ><br><br>
         </div>
         <div >
           <label for="resInc3">Respuesta incorrecta 3</label>
-            <input type="text" id="resInc3" >
+            <input type="text" id="resInc3" ><br><br>
         </div>
         <div>
           <label for="dificultad">Selecciona dificultad</label>
@@ -45,22 +44,27 @@
             <option value="media">Media</option>
             <option value="dificl">Dificil</option>
           </select>
+          <br><br>
         </div>
         <div>
         <label for="tema">Tema</label>
-            <input type="text" id="tema" >
+            <input type="text" id="tema" ><br><br>
         </div>
         <div>
           <label for="imagen">Selecciona una imagen</label>
           <input type="file" name="imagen" id="imagen">
+        </div>
+        <div>
           <img id="previewImagen" src="#" width="100" height="100"/>
         </div>
+        <br><br>
         <div>
           <input type="button" value="Submit" id="submit_b">
         </div>
       </form>
     </div>
   </section>
+  
   <?php include '../html/Footer.html' ?>
   <script src="../js/ValidateFieldsQuestion.js"></script>
   <script src="../js/ShowImageInForm.js"></script>
