6f695d36681ac225428d16ac34b8941ce2b8d2e2
Gorka Alvarez <galvarez024@ikasle.ehu.eus>
2019-10-13 11:44:52 +0200

    añadir pregunta a BD Terminado

--- php/QuestionFormHtml5.php
+++ php/QuestionFormHtml5.php
@@ -10,27 +10,27 @@
   <section class="main" id="s1">
     <div>
 
-    <form action="AddQuestion.php" name="fquestion" id="fquestion">
+    <form action="AddQuestion.php" name="fquestion" id="fquestion" method="post" enctype=multipart/form-data>
             <p>Introduce tu dirección de correo: *</p>
-            <input type="email" size="60" id="dirCorreo" name="Direccion Correo Electronico" required pattern="((^[a-zA-Z]+(([0-9]{3})+@ikasle\.ehu\.(eus|es))$)|^[a-zA-Z]+(\.[a-zA-Z]+@ehu\.(eus|es)|@ehu\.(eus|es))$)">
+            <input type="email" size="60" id="dirCorreo" name="dirCorreo" required pattern="((^[a-zA-Z]+(([0-9]{3})+@ikasle\.ehu\.(eus|es))$)|^[a-zA-Z]+(\.[a-zA-Z]+@ehu\.(eus|es)|@ehu\.(eus|es))$)">
             <p>Introduce el enunciado de la pregunta: *</p>
-            <input type="text" size="60" id="nombrePregunta" name="Enunciado Pregunta" required pattern="^.{10,}$">
+            <input type="text" size="60" id="nombrePregunta" name="nombrePregunta" required pattern="^.{10,}$">
             <p>Respuesta correcta: *</p>
-            <input type="text" size="60" id="respuestaCorrecta" name="Respuesta Correcta" required>
+            <input type="text" size="60" id="respuestaCorrecta" name="respuestaCorrecta" required>
             <p>Respuesta incorrecta1: *</p>
-            <input type="text" size="60" id="respuestaIncorrecta1" name="Respuesta Incorrecta 1" required>
+            <input type="text" size="60" id="respuestaIncorrecta1" name="respuestaIncorrecta1" required>
             <p>Respuesta incorrecta2: *</p>
-            <input type="text" size="60" id="respuestaIncorrecta2" name="Respuesta Incorrecta 2" required>
+            <input type="text" size="60" id="respuestaIncorrecta2" name="respuestaIncorrecta2" required>
             <p>Respuesta incorrecta3: *</p>
-            <input type="text" size="60" id="respuestaIncorrecta3" name="Respuesta Incorrecta 3" required>
+            <input type="text" size="60" id="respuestaIncorrecta3" name="respuestaIncorrecta3" required>
             <p>Complejidad de la pregunta: *</p>
-            <select id="complejidad" >
-                <option value="baja">Baja</option>
-                <option value="media" selected>Media</option>
-                <option value="alta">Alta</option>
+            <select id="complejidad" name="complejidad" >
+                <option value="1">Baja</option>
+                <option value="2" selected>Media</option>
+                <option value="3">Alta</option>
             </select>
             <p>Introduce el tema de la pregunta: *</p>
-            <input type="text" size="60" id="temaPregunta" name="Tema de la Pregunta" required>
+            <input type="text" size="60" id="temaPregunta" name="temaPregunta" required>
             <div id="selector">
             <input type="file" id="file" accept="image/*" name="Imagen" required>
             </div>
