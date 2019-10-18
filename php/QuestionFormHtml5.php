c837b7910ddba6ec35e0e26703fa4bfe1050eed1
Gorka Alvarez <galvarez024@ikasle.ehu.eus>
2019-10-18 13:27:12 +0200

    Validacion en servidor

--- php/QuestionFormHtml5.php
+++ php/QuestionFormHtml5.php
@@ -12,17 +12,17 @@
 
     <form action="AddQuestionWithImage.php" name="fquestion" id="fquestion" method="post" enctype="multipart/form-data">
             <p>Introduce tu dirección de correo: *</p>
-            <input type="email" size="60" id="dirCorreo" placeholder="usuario@ehu.es" name="dirCorreo" required pattern="((^[a-zA-Z]+(([0-9]{3})+@ikasle\.ehu\.(eus|es))$)|^[a-zA-Z]+(\.[a-zA-Z]+@ehu\.(eus|es)|@ehu\.(eus|es))$)">
+            <input type="email" size="60" id="dirCorreo" name="dirCorreo" required >
             <p>Introduce el enunciado de la pregunta: *</p>
-            <input type="text" size="60" id="nombrePregunta" placeholder="Enunciado Pregunta" name="nombrePregunta" required pattern="^.{10,}$">
+            <input type="text" size="60" id="nombrePregunta" name="nombrePregunta" required>
             <p>Respuesta correcta: *</p>
-            <input type="text" size="60" id="respuestaCorrecta" placeholder="Respuesta Correcta" name="respuestaCorrecta" required>
+            <input type="text" size="60" id="respuestaCorrecta" name="respuestaCorrecta" required>
             <p>Respuesta incorrecta1: *</p>
-            <input type="text" size="60" id="respuestaIncorrecta1" placeholder="Respuesta Incorrecta 1" name="respuestaIncorrecta1" required>
+            <input type="text" size="60" id="respuestaIncorrecta1" name="respuestaIncorrecta1" required>
             <p>Respuesta incorrecta2: *</p>
-            <input type="text" size="60" id="respuestaIncorrecta2" placeholder="Respuesta Incorrecta 2" name="respuestaIncorrecta2" required>
+            <input type="text" size="60" id="respuestaIncorrecta2" name="respuestaIncorrecta2" required>
             <p>Respuesta incorrecta3: *</p>
-            <input type="text" size="60" id="respuestaIncorrecta3" placeholder="Respuesta Incorrecta 3" name="respuestaIncorrecta3" required>
+            <input type="text" size="60" id="respuestaIncorrecta3" name="respuestaIncorrecta3" required>
             <p>Complejidad de la pregunta: *</p>
             <select id="complejidad" name="complejidad" >
                 <option value="1">Baja</option>
@@ -30,13 +30,14 @@
                 <option value="3">Alta</option>
             </select>
             <p>Introduce el tema de la pregunta: *</p>
-            <input type="text" size="60" id="temaPregunta" placeholder="Tema de la pregunta" name="temaPregunta" required>
+            <input type="text" size="60" id="temaPregunta" name="temaPregunta" required>
             <div id="selector">
             <input type="file" id="file" accept="image/*" name="Imagen" required>
             </div>
             <p> <input type="submit" id="submit" value="Enviar"> <input type="reset" value="Limpiar"></p>
         </form>
     </div>
+      
   </section>
   <?php include '../html/Footer.html' ?>
 </body>
