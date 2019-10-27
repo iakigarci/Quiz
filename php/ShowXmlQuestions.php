a833cef2aee2b73e3b65ff8c547701f86fdb2ca8
iakigarci <garcianoya@hotmail.es>
2019-10-27 21:24:15 +0100

    Tarea 2 realizada

--- php/ShowXmlQuestions.php
+++ php/ShowXmlQuestions.php
@@ -10,7 +10,21 @@
     <section class="main" id="s1">
         <div>
             <?php
-            $assessmentItems
+            echo "<table border='1'>";
+            echo "<tr>";
+            echo "<th>Autor</th>";
+            echo "<th>Enunciado</th>";
+            echo "<th>Respuesta Correcta</th>";
+            echo "</tr>";
+            $assessmentItems = simplexml_load_file('../xml/Questions.xml');
+            foreach ($assessmentItems->xpath('//assessmentItem') as $assessmentItem) {
+                echo "<tr>";
+                echo "<td>{$assessmentItem['author']}</td>";
+                echo "<td>{$assessmentItem->itemBody->p}</td>";
+                echo "<td>{$assessmentItem->correctResponse->value}</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
             ?>
         </div>
     </section>
