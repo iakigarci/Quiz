a48dd312e25ebb9c2b27f040c5f318d6406401f4
iakigarciaL <garcianoya@hotmail.es>
2019-10-31 12:31:41 +0100

    Funciona GetUserInfo.html

--- js/GetUserInfo.js
+++ js/GetUserInfo.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
-    $.get("../xml/Users.xml",function (d) {
-        var listaUsuarios = $(d).find('email');
-        var salir = false;
-        while (!salir) {
-            if (listaUsuarios[i].childNodes[0].nodeValue == $("#dirCorreo").val()) {
-                salir = True;
-                alert(listaUsuarios[i].childNodes[0].nodeValue);
-            }else{
-                i++;
-            }
-        }   
-    })
+    $("#completar").click(function () {
+        $.get("../xml/Users.xml",function (d) {
+            $(d).find('usuario').each(function () {
+                var email = $(this).find('email').text();
+                if (email==$("#dirCorreo").val()) {
+                    $("#nombre").val($(this).find('nombre').text());
+                    $("#apellido").val($(this).find('nombre').text()+" "+$(this).find('apellido1').text()+" "+$(this).find('apellido2').text());
+                    $("#telefono").val($(this).find('telefono').text());
+                }
+            });
+        }); 
+    });  
+    $("#dirCorreo").focus(function () {
+        $("#nombre").val("");
+        $("#telefono").val("");
+        $("#apellido").val("");
+    });      
 });
